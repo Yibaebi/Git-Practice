fa059a78ad079228f5fc6586e6f929310f132288
Yibaebi Elliot <59321440+Yibaebi@users.noreply.github.com>
2020-10-11 15:06:58 +0100

    Initial Commit

--- src/ReacRouter/Components/Home.js
+++ src/ReacRouter/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
       <p>
-        Love is a beautiful thing. Learn to love the Lord.
+        Love is a beautiful thing. Learn to love the Lord with all your heart and soul.
       </p>
     </div>
   );
